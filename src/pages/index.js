fc8918bdf7082996ffc04b949f5efb05e71a3ff4
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-07-08 15:26:23 +0530

    Add ChessBoard Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
-export default function HomePage(){
-    return (
-        <div className="flex justify-center items-center min-h-full bg-red-400">
-            Home Page
-        </div>
-    )
+import { ChessBoard } from "@/components/ChessBoard";
+
+export default function HomePage() {
+  return (
+    <div className="flex justify-center items-center min-h-full bg-red-400">
+      <ChessBoard />
+    </div>
+  );
 }
