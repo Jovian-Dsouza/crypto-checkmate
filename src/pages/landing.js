fe2f248ac0beae97a2e8be46d5c682e5d5deddef
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-07-11 22:24:15 +0530

    Add Landing page to frontend

---
+++ src/pages/landing.js
@@ -0,0 +1,89 @@
+const Frame = () => {
+  return (
+    <div className="relative bg-black shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-full h-[4894px] overflow-hidden text-left text-45xl text-white font-manrope">
+      <img
+        className="absolute top-[0px] left-[0px] w-full h-[1119px] object-cover"
+        alt=""
+        src="/imgs/background.png"
+      />
+      <div className="absolute w-[32.78%] top-[76px] right-[5.91%] left-[61.32%] rounded-[100px] bg-violet [filter:blur(500px)] h-[642px]" />
+      <b className="absolute top-[597px] left-[178px] text-21xl leading-[50px] inline-block w-[782px] h-[86px]">
+        <p className="m-0">Play Decentralised Chess on FlowChain,</p>
+        <p className="m-0 [background:linear-gradient(180deg,_#f4a100,_#c400e4_89.06%)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
+          Compete, Conquer, and Earn!
+        </p>
+      </b>
+      <b className="absolute w-[80.6%] top-[310px] left-[-7.85%] text-[100px] tracking-[0.48px] leading-[125px] inline-block whitespace-pre-wrap">
+        <p className="m-0">{`               Welcome to `}</p>
+        <p className="m-0"> CryptoKnights</p>
+      </b>
+      <div className="absolute w-[60.62%] top-[1190px] right-[50%] left-[-10.62%] rounded-981xl [background:linear-gradient(180deg,_rgba(95,_95,_95,_0),_#72ff91)] shadow-[0px_58px_80px_rgba(0,_0,_0,_0.16)] h-[1050px]" />
+      <div className="absolute w-[60.62%] top-[2190px] right-[-10.62%] left-[50%] rounded-981xl [background:linear-gradient(180deg,_#694091,_rgba(25,_22,_28,_0))] shadow-[0px_58px_80px_rgba(0,_0,_0,_0.16)] h-[1050px]" />
+      <div className="absolute w-[60.62%] top-[3199px] right-[48.04%] left-[-8.66%] rounded-981xl [background:linear-gradient(180deg,_#7c3400,_rgba(164,_69,_0,_0))] shadow-[0px_58px_80px_rgba(0,_0,_0,_0.16)] h-[1050px]" />
+      <div className="absolute top-[4763px] left-[0px] [background:linear-gradient(180deg,_rgba(41,_1,_91,_0.71),_rgba(15,_109,_103,_0.23),_#000)] w-[1732px] h-[131px]" />
+      <img
+        className="absolute top-[197px] left-[978px] w-[736px] h-[800px] object-cover"
+        alt=""
+        src="/imgs/logo.png"
+      />
+      <div className="absolute top-[0px] left-[0px] bg-black w-[1732px] h-[100px]" />
+      <div className="absolute top-[752px] left-[179px] rounded-xl bg-lightgreen w-[383px] h-[87px]" />
+      <b className="absolute top-[770px] left-[223px] text-21xl leading-[50px] inline-block text-black w-72 h-[42px]">
+        Get Started ....
+      </b>
+      <img
+        className="absolute top-[2371px] left-[941px] w-[927px] h-[931px] object-cover"
+        alt=""
+        src="/imgs/make-your-move.png"
+      />
+      <img
+        className="absolute top-[calc(50%_-_1127px)] left-[calc(50%_-_866px)] w-[914px] h-[790px] overflow-hidden object-cover"
+        alt=""
+        src="/imgs/play-chess-your-way.png"
+      />
+      <img
+        className="absolute top-[calc(50%_+_855px)] left-[calc(50%_-_866px)] w-[976px] h-[843px] overflow-hidden object-cover"
+        alt=""
+        src="/imgs/chessBoardMultiplayer.png"
+      />
+      <b className="absolute top-[4817px] left-[953px] text-[24px] leading-[25.2px] inline-block w-[736px] h-[51px]">
+        Made with 💖 for the Web3 Community by Team CryptoKnights
+      </b>
+      <div className="absolute top-[10px] left-[70px] w-[414px] h-[93px] text-17xl">
+        <img
+          className="absolute top-[0px] left-[0px] w-[86px] h-[93px] object-cover"
+          alt=""
+          src="/imgs/logo.png"
+        />
+        <b className="absolute top-[35px] left-[96px] leading-[25.2px] inline-block w-[318px] h-[37px]">
+          ChecKnights
+        </b>
+      </div>
+      <div className="absolute top-[1483px] left-[1062px] leading-[20px] font-medium font-space-grotesk flex items-center w-[684px] h-[259px]">
+        <span className="[line-break:anywhere] w-full">
+          <p className="[margin-block-start:0] [margin-block-end:60px]">{`Ultimate Platform `}</p>
+          <p className="[margin-block-start:0] [margin-block-end:60px]">{`for Multiplayer `}</p>
+          <p className="m-0">{`betting `}</p>
+        </span>
+      </div>
+      <div className="absolute top-[3465px] left-[1112px] leading-[20px] font-medium font-space-grotesk flex items-center w-[684px] h-[259px]">
+        <span className="[line-break:anywhere] w-full">
+          <p className="[margin-block-start:0] [margin-block-end:60px]">{`Ultimate Platform `}</p>
+          <p className="[margin-block-start:0] [margin-block-end:60px]">{`for Multiplayer `}</p>
+          <p className="m-0">{`betting `}</p>
+        </span>
+      </div>
+      <div className="absolute top-[2525px] left-[148px] leading-[20px] font-medium font-space-grotesk flex items-center w-[684px] h-[259px]">
+        <span className="[line-break:anywhere] w-full">
+          <p className="[margin-block-start:0] [margin-block-end:60px]">{`Ultimate Platform `}</p>
+          <p className="[margin-block-start:0] [margin-block-end:60px]">{`for Multiplayer `}</p>
+          <p className="m-0">{`betting `}</p>
+        </span>
+      </div>
+      <div className="absolute top-[12px] left-[1456px] rounded-xl bg-orange w-[209px] h-[76px]" />
+      <div className="absolute top-[23px] left-[1496px] text-17xl leading-[50px] text-black inline-block w-44 h-9">{`login ->`}</div>
+    </div>
+  );
+};
+
+export default Frame;
