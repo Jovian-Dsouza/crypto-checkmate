f75c960d89c0bcfb35e6748b1a6adb6d5235342a
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-07-12 09:15:28 +0530

    Add Landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: "#000",
+        orange: "#fca311",
+        white: "#fff",
+        lightgreen: "#7ad05b",
+        violet: "rgba(240, 135, 255, 0.3)",
+      },
+      fontFamily: {
+        manrope: "Manrope",
+        "space-grotesk": "'Space Grotesk'",
+      },
+      borderRadius: {
+        xl: "20px",
+        "981xl": "1000px",
       },
     },
+    // fontSize: {
+    //   "17xl": "36px",
+    //   "45xl": "64px",
+    //   "21xl": "40px",
+    // },
+  },
+  corePlugins: {
+    preflight: false,
   },
   plugins: [],
-}
+};
