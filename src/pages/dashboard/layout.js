9cc7985d76070dc88016a45b1f74ab5220853c21
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-07-15 16:01:30 +0530

    Add dashboard UI

---
+++ src/pages/dashboard/layout.js
@@ -0,0 +1,10 @@
+export default function DashboardLayout({ children }) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      <nav>This is a dashboard DashboardLayout</nav>
+
+      {/* {children} */}
+    </section>
+  );
+}
